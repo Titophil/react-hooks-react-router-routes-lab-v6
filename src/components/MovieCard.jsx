c4d23675d971866a9deb47cdb4ae6388815e5f60
titophil <kipronotitus254@gmail.com>
2025-04-23 10:38:06 +0300

    everything

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,13 @@
-import {Link} from 'react-router-dom';
 
-function MovieCard({title}) {
-  
+import { Link } from 'react-router-dom';
+
+function MovieCard({ title }) {
   return (
     <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
+        <h2>{title.title}</h2> {/* Access the actual title string */}
+        <Link to={`/movie/${title.id}`}>View Info</Link> {/* Access the ID */}
     </article>
   );
-};
+}
 
 export default MovieCard;
